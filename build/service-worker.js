73e04bb7028e6826637c3e87aa7477c1f21be13c
roxyvaught <roxanna.vaught@gmail.com>
2020-09-10 17:21:25 -0500

    link update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.fdbf8af2387b33ee89ff5aa262a724da.js"
+  "/precache-manifest.1cf3c37a9edf4e0e6c32658c63b905b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,14 +12,14 @@ function About() {
                     <div className="col-10 section-content">
                         <img className="mt-3 mb-5 me" id="profile" src={profile} alt="Roxanna Vaught"/>
                         <p id="aboutp">
-                        I am a geologist and web developer with a diverse background in oil and gas. I recently obtained my Web Development Certificate from the University of Texas at Austin and I hold Bachelor's and Master's degrees in geology. During graduate school and my time since then, I was lucky enough to work with the US Bureau of Land Management, TGS-NOPEC, and as a Wireline Field Engineer for Schlumberger. 
+                        I am a geologist and web developer with a diverse background in oil and gas. I recently obtained my Web Development Certificate from the University of Texas at Austin and I hold Bachelor's and Master's degrees in geology. During graduate school and my time since graduating, I have been lucky enough to work with the US Bureau of Land Management, TGS-NOPEC, and as a Wireline Field Engineer for Schlumberger. 
                         </p>
                         <p>
                         My strengths in the workplace stem from my ability to build strong teams, find creative solutions, and adapt to changing circumstances in high-pressure environments. I thrive in challenging and diverse environments and am on the lookout for my next career adventure! 
                         </p>
 
                         <p>
-                            <a href="/#/resume" className="text-info text-decoration-none"> View my resume |</a>
+                            <a href="https://github.com/roxyvaught/react-portfolio/raw/master/src/assets/resume.pdf" className="text-info text-decoration-none"> View my resume |</a>
                             <a href="mailto:roxanna.vaught@gmail.com" className="text-primary text-decoration-none"> Send me an email</a>
                         </p>
                     </div>  
