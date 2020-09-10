73068b94c9863d3c246594aee39841a820180754
roxyvaught <roxanna.vaught@gmail.com>
2020-09-10 16:56:36 -0500

    Updates

---
+++ precache-manifest.1cf3c37a9edf4e0e6c32658c63b905b7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b903a8a3e2173659880dd0c464ac69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0479f21267e454ba08ba",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "eff0c3a141ff4ca2a79a",
+    "url": "/static/css/main.bdf8a113.chunk.css"
+  },
+  {
+    "revision": "0479f21267e454ba08ba",
+    "url": "/static/js/2.8c5bb31f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8c5bb31f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff0c3a141ff4ca2a79a",
+    "url": "/static/js/main.ee1d27e4.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "e80c307cb904cc92bce054a68c9fdb9f",
+    "url": "/static/media/roxanna.e80c307c.JPG"
+  },
+  {
+    "revision": "b0a7e2ede85157912f3b0dad5cae94af",
+    "url": "/static/media/splashbw.b0a7e2ed.jpg"
+  }
+]);
